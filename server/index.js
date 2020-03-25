9574400ec08de27e8a1a6bbcf06607d59a4dbc24
Dahan Schuster <dan.plschuster@gmail.com>
2020-03-25 01:41:04 -0300

    Move o arquivo Index para a pasta src/

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get('/', (request, response) => response.json({
-	'evento': 'Semana Omnistack 11.0',
-	'aluno': 'Dahan Schuster'
-}))
-
-app.listen(3333)
