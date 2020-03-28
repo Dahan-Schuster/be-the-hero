8a1a5ff20a774058535832378a898d7ef269ae0c
Dahan Schuster <dan.plschuster@gmail.com>
2020-03-27 23:16:00 -0300

    Configura a função que instancia o router e a renderização das páginas

--- web/src/App.js
+++ web/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './assets/css/global.css'
+import Routes from './routes'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	
+	/**
+	 * Elementos e variáveis React são imutáveis. Uma vez criados,
+	 * você não pode alterar seus elementos filhos ou atributos.
+	 *
+	 * Então como eu altero valores de variáveis? Serão sempre constantes?
+	 * Vou precisar ficar renderizando tudo denovo com valores diferentes?
+	 * Éooqqqq??? Não!
+	 *
+	 * No React, variáveis e elementos (objetos JSX) são STATEFUL
+	 * Isto significa que eles possuem ESTADO. O a variável em si
+	 * não é alterada, mas sim seu estado atual.
+	 *
+	 * O método useState é utilizado para criar uma variável com um
+	 * valor inicial e obter um método de atualização de seu estado
+	 *
+	 * Para componentes, é necessário criá-los a partir de classes
+	 * (class Componente extends React.Component) e utilizar os objetos
+	 * this.state e this.setState. O this.state deve ser inicializado
+	 * no construtor com os 'atributos' do componente que estão passíveis
+	 * de alteração (como a hora atual de um timer); o this.setState
+	 * deve ser usado para alterar os valores definidos em this.state
+	 * Ex.:
+	 *      this.state = {timer: 0} (no construtor)
+	 *      this.setState({ timer: new Date() }) (em outros métodos)
+	 */
+	
+	return (<Routes/>)
 }
 
-export default App;
+export default App
