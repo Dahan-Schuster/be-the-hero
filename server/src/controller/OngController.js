cc2933a7bcfd5851a13108aa49cd3993ddae120c
Dahan Schuster <dan.plschuster@gmail.com>
2020-03-29 15:16:48 -0300

    Exporta a função de geração de IDs para um arquivo

--- server/src/controller/OngController.js
+++ server/src/controller/OngController.js
@@ -1,20 +1,25 @@
-const crypto = require('crypto')
+const generateUniqueId = require('../utils/generateUniqueId')
 const conexao = require('./../database/conexao')
 
 module.exports = {
-	async index(request, response) {
-		const ongs = await conexao('ong_ong').select('*')
-		return response.json(ongs)
-	},
-	
-	async cadastrar(request, response) {
-		const {ong_nome, ong_email, ong_whatsapp, ong_cidade, ong_uf} = request.body
-		const ong_id = crypto.randomBytes(4).toString('HEX')
-		
-		await conexao('ong_ong').insert({
-			ong_id, ong_nome, ong_email, ong_whatsapp, ong_cidade, ong_uf,
-		})
-		
-		return response.json({ong_id})
-	},
+    async index(request, response) {
+        const ongs = await conexao('ong_ong').select('*')
+        return response.json(ongs)
+    },
+
+    async cadastrar(request, response) {
+        const { ong_nome, ong_email, ong_whatsapp, ong_cidade, ong_uf } = request.body
+        const ong_id = generateUniqueId()
+
+        await conexao('ong_ong').insert({
+            ong_id,
+            ong_nome,
+            ong_email,
+            ong_whatsapp,
+            ong_cidade,
+            ong_uf,
+        })
+
+        return response.json({ ong_id })
+    },
 }
